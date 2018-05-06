d45a0860a3609f26fc57bd129519c4e67a50c5ab
NgocQuynh1206 <36333470+NgocQuynh1206@users.noreply.github.com>
2018-05-06 17:54:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// enable cors
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "*");
+	next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/api', celebrities);
